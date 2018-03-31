ce89646f11dbca1661a8b3a88d83dad1654703b5
kspark <vhehentm@gmail.com>
2018-03-31 12:10:20 +0900

    관리자페이지
    뉴스 글쓰기

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,13 +5,16 @@ import { HomeComponent } from './home/home.component';
 import { NewsComponent } from './news/news.component';
 import {RouterModule, Routes} from '@angular/router';
 import {
-  MatButtonModule, MatCardModule, MatExpansionModule, MatIconModule, MatPaginatorModule,
+  MatButtonModule, MatCardModule, MatExpansionModule, MatFormFieldModule, MatIconModule, MatInputModule,
+  MatPaginatorModule,
   MatToolbarModule
 } from '@angular/material';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {AdminService} from './admin.service';
 import { ViewComponent } from './news/view/view.component';
 import { WriteComponent } from './news/write/write.component';
+import {CKEditorModule} from "ng2-ckeditor";
+import {FormsModule} from "@angular/forms";
 
 const routes: Routes = [
   {path: '', component: AdminComponent, children: [
@@ -26,14 +29,18 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(routes),
     FlexLayoutModule,
+    CKEditorModule,
     MatToolbarModule,
     MatExpansionModule,
     MatCardModule,
     MatPaginatorModule,
     MatButtonModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   declarations: [AdminComponent, HomeComponent, NewsComponent, ViewComponent, WriteComponent],
   providers: [AdminService]
